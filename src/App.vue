9337fd254c5554e6949fae0dba7cc42bcfe51dd7
TheWisePigeon <zozozozeph@gmail.com>
2022-04-04 21:22:55 +0000

    set up tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import TheHeader from "./components/TheHeader.vue";
+ export default {
+   components:{
+     TheHeader: TheHeader
+   }
+ }
 </script>
 
 <template>
-  Hello HelloWorld
+  <div>
+    <TheHeader/>
+  </div>
 </template>
 
 <style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<template>
+  <div class="header bg-red-900">
+      Test bruh
+  </div>
+</template>
+
+<style scoped>
+    .header{
+        width: 100%;
+        height: 150px;
+    }
+</style>
