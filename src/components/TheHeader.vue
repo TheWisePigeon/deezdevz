ed034a2489fb0a5f6c3dbbd954f48feb7615050f
TheWisePigeon <zozozozeph@gmail.com>
2022-04-04 22:19:43 +0000

    implemented dark/light mode switch on/off

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,41 @@
 <script>
+export default {
+  data() {
+    return {
+      lightMode: true,
+    };
+  },
+  computed: {
+    darkModeClass() {
+      return {
+        "dark:bg-slate-900 dark:text-white": this.lightMode === true,
+      };
+    },
+    mode(){
+        return this.lightMode ? 'Light Mode' : 'Dark Mode'
+    }
+  },
+  methods:{
+      toggleLightMode(){
+          this.lightMode = !this.lightMode
+          console.log('Bruh');
+      }
+  }
+};
 </script>
 
 <template>
-  <div class="header bg-red-900">
-      Test bruh
+  <div class="header bg-white" :class="darkModeClass">
+      <h1>
+          Bruh, welcome to DeezDevz
+      </h1>
+      <button class=" bg-red-600" @click="toggleLightMode">{{mode}}</button>
   </div>
 </template>
 
 <style scoped>
-    .header{
-        width: 100%;
-        height: 150px;
-    }
+.header {
+  width: 100%;
+  height: 150px;
+}
 </style>
