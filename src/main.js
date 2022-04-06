1666a454668b84eba6d96b3a9f7ef021d9b5bdf8
TheWisePigeon <zozozozeph@gmail.com>
2022-04-06 07:49:31 +0000

    moving to gridsome

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "./tailwindconfig/tailwind.css";
-
-createApp(App).mount('#app')
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/components/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
