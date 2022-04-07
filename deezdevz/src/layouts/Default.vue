56bc7bb55c61f2c3c15ee4a809b9e9447c4ad559
TheWisePigeon <zozozozeph@gmail.com>
2022-04-07 23:21:24 +0000

    Update Default.vue
    added buttons toggling effect

--- deezdevz/src/layouts/Default.vue
+++ deezdevz/src/layouts/Default.vue
@@ -3,11 +3,37 @@
     <header class="flex justify-between py-6">
       <g-link class="text-3xl" to="/">{{ $static.metadata.siteName }}</g-link>
       <div class="flex items-center">
-        <div v-if="this.theme === 'Dark'">
-          <DarkTheme />
+        <div v-if="this.darkTheme">
+          <button @click="changeTheme">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              />
+            </svg>
+          </button>
         </div>
         <div v-else>
-          <LightTheme />
+          <button @click="changeTheme" >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </header>
@@ -30,25 +56,27 @@ export default {
   },
   data() {
     return {
-      theme
+      darkTheme: null,
     };
   },
   beforeMount() {
-    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
       document.body.classList.add("bg-slate-900");
       document.body.classList.add("text-white");
+      this.darkTheme = true;
     } else {
       document.body.classList.add("bg-white");
+      this.darkTheme = false;
     }
   },
   methods: {
     changeTheme() {
-      if (this.theme === "Dark") {
-        this.theme = "Light"
-      } 
-      if (this.theme==="Light") {
-        this.theme = "Dark"
-      }
+      console.log(this.darkTheme);
+      this.darkTheme = !this.darkTheme
+      console.log(this.darkTheme);
     },
   },
 };
